6b44097ecda7a62b424a91e263d56cc767862bfc
rockerbacon <vas.vitor.santos@gmail.com>
2019-10-31 23:39:17 -0300

    Generated version without tests

--- .assertions/dependency_manager/install.sh
+++ .assertions/dependency_manager/install.sh
@@ -2,4 +2,3 @@
 apt-package/install.sh g++
 apt-package/install.sh cmake
 apt-package/install.sh make
-git/install.sh https://github.com/rockerbacon/assertions-test.git b15ea6aeb8f0145fedbe0b1565e1ba5acb3750d3
