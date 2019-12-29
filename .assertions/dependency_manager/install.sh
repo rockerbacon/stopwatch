5b3bd1026ea63431e795e536d6c5f180d6b9df9d
rockerbacon <vas.vitor.santos@gmail.com>
2019-12-29 20:06:42 -0300

    Updated assertions

--- .assertions/dependency_manager/install.sh
+++ .assertions/dependency_manager/install.sh
@@ -2,4 +2,4 @@
 apt-package/install.sh g++
 apt-package/install.sh cmake
 apt-package/install.sh make
-git/install.sh https://github.com/rockerbacon/assertions-test.git v1.0 true "src/objs" "src/objs"
+git/install.sh https://github.com/rockerbacon/assertions-test.git 2166d534f1fe1e9e88b15383409ec073aebe3198 true "src/objs" "src/objs" ""
